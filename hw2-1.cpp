bfeb97f5b009230a217ccde6d66f824414724186
Ernest728 <41069663+Ernest728@users.noreply.github.com>
2023-03-29 01:25:04 +0800

    Update hw2-1.cpp

--- hw2-1.cpp
+++ hw2-1.cpp
@@ -3,7 +3,7 @@ PwmOut PWM1(D6);
 // main() runs in its own thread in the OS
 int main()
 {
-    PWM1.period_ms(50);
+    PWM1.period_ms(5);
     float R[20];
     int Re[20];
     int i = 0;
@@ -17,7 +17,7 @@ int main()
     for (;i < 20; i++)
         R[i] = 0.0;
     for (i = 0; i < 20; i++)
-        Re[i] = int(R[i]*50000);
+        Re[i] = int(R[i]*5000);
     while (true) {
         for (int j = 0; j < 20; j++) {
             PWM1.pulsewidth_us(Re[j]);
